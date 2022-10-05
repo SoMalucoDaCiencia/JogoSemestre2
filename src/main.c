4ee7da45dd11e2f3b25c00a4891472010206199b
A.Andrade <arthurandrade@controlese.com.br>
2022-10-05 11:32:54 -0300

    Testes de FPS e Clicks

--- src/main.c
+++ src/main.c
@@ -10,29 +10,53 @@
 #include <allegro5/allegro_image.h>
 #include <unistd.h>
 #include <allegro5/allegro_primitives.h>
-#define background "background.bmp"
 
 int main() {
 
     al_init();
-    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-    event_queue = al_create_event_queue();
+    al_init_image_addon();
+    al_init_primitives_addon();
 
+    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
     ALLEGRO_DISPLAY* display = al_create_display(1280,720);
+
+    event_queue = al_create_event_queue();
     al_register_event_source(event_queue, al_get_display_event_source(display));
 
-    //ALLEGRO_BITMAP *image = al_load_bitmap(background);
-    //al_draw_bitmap(image, 0, 0, 0);
-    //al_destroy_bitmap(image);
+    // Inicia loops por FPS
+    float FPS = 60;
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / FPS);
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_start_timer(timer);
+
+    // Inicia eventos criadsos pelo mouse
+    al_install_mouse();
+    al_register_event_source(event_queue, al_get_mouse_event_source());
+
+    // Inicia eventos criadsos pelo teclado
+    al_install_keyboard();
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+
     while (true) {
-        //handle input and timer
         ALLEGRO_EVENT ev;
         al_wait_for_event(event_queue, &ev);
 
-        if (ev.type == ALLEGRO_EVENT_TIMER) {
-            //put all fps dependant function here
-        } else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            break;
+        switch (ev.type) {
+            case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN: {
+                printf("Click\n");
+                break;
+            }
+            case ALLEGRO_EVENT_TIMER: {
+                printf("FPS rodando\n");
+                break;
+            }
+            case ALLEGRO_EVENT_DISPLAY_CLOSE: {
+                al_destroy_timer(timer);
+                al_destroy_event_queue(event_queue);
+                al_destroy_display(display);
+                exit(0);
+            }
         }
     }
 }
