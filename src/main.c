3cac42966aefafc2da460081065d50d0acf97293
A.Andrade <arthurandrade@controlese.com.br>
2022-10-05 10:44:27 -0300

    Fechamento de janela

--- src/main.c
+++ src/main.c
@@ -15,14 +15,24 @@
 int main() {
 
     al_init();
-//    al_init_image_addon();
-//    al_init_primitives_addon();
+    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+    event_queue = al_create_event_queue();
 
-    al_create_display(1280,720);
+    ALLEGRO_DISPLAY* display = al_create_display(1280,720);
+    al_register_event_source(event_queue, al_get_display_event_source(display));
 
     //ALLEGRO_BITMAP *image = al_load_bitmap(background);
     //al_draw_bitmap(image, 0, 0, 0);
     //al_destroy_bitmap(image);
-    while(1==1){
+    while (true) {
+        //handle input and timer
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue, &ev);
+
+        if (ev.type == ALLEGRO_EVENT_TIMER) {
+            //put all fps dependant function here
+        } else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+            break;
+        }
     }
 }
