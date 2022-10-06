06084356ae88dbdad43abd3ed32a4d68b173121d
davisilvahenrique <davisilvahenrique@gmail.com>
2022-10-06 03:06:20 -0300

    .

--- src/main.c
+++ src/main.c
@@ -10,8 +10,6 @@
 #include <allegro5/allegro_image.h>
 #include <unistd.h>
 #include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
 
 static bool FPS_POLARITY = false;
 static const float FPS = 60;
@@ -22,6 +20,8 @@ int main() {
 
     ALLEGRO_EVENT_QUEUE *event_queue = NULL;
     ALLEGRO_DISPLAY* display = al_create_display(1280,720);
+
+    al_init_image_addon();
 
     event_queue = al_create_event_queue();
     al_register_event_source(event_queue, al_get_display_event_source(display));
@@ -39,16 +39,14 @@ int main() {
     al_install_keyboard();
     al_register_event_source(event_queue, al_get_keyboard_event_source());
 
-
     while (true) {
         ALLEGRO_EVENT ev;
-        ALLEGRO_BITMAP *background, *play, *options, *quit;
+        ALLEGRO_BITMAP *bg = NULL, *play = NULL, *options = NULL, *quit = NULL;
+
         al_wait_for_event(event_queue, &ev);
 
         switch (ev.type) {
             case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN: {
-//                printf("Click\n");
-//                printf("%d\n",ev.mouse.x);
 
                 if (ev.mouse.x >= 490 && ev.mouse.x <= 790) {
 
@@ -59,7 +57,7 @@ int main() {
                         exit(0);
                     } else if (ev.mouse.y >= 300 && ev.mouse.y <= 400) {     // config
                         printf("config\n");
-                    } else if (ev.mouse.y >= 150 && ev.mouse.y <= 300) {     // play
+                    } else if (ev.mouse.y >= 150 && ev.mouse.y <= 250) {     // play
                         printf("play\n");
                     }
 
@@ -68,8 +66,15 @@ int main() {
             }
             case ALLEGRO_EVENT_TIMER: {
                 FPS_POLARITY = !FPS_POLARITY;
-//                background = al_load_bitmap("assets/background.png");
-//                al_draw_bitmap(background,0,0,0);
+                bg = al_load_bitmap("assets/background.png");
+                al_draw_bitmap(bg,0,0,0);
+                play = al_load_bitmap("assets/play.png");
+                al_draw_bitmap(play,490,150,0);
+                options = al_load_bitmap("assets/options.png");
+                al_draw_bitmap(options,490,300,0);
+                quit = al_load_bitmap("assets/quit.png");
+                al_draw_bitmap(quit,490,450,0);
+                al_flip_display();
                 insertSquare(100, 300, 490, 150, al_map_rgb(237, 66, 69), display);
                 insertSquare(100, 300, 490, 300, al_map_rgb(237, 66, 69), display);
                 insertSquare(100, 300, 490, 450, al_map_rgb(237, 66, 69), display);
