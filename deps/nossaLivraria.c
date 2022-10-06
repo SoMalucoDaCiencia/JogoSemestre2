8547eed939d6db1d026e653a65ece186e90da50d
A.Andrade <arthur10andrade@yahoo.com.br>
2022-10-05 22:46:22 -0300

    .draw square

--- deps/nossaLivraria.c
+++ deps/nossaLivraria.c
@@ -3,10 +3,26 @@
 //
 
 #include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_image.h>
+#include <unistd.h>
+#include <allegro5/allegro_primitives.h>
 
 bool isMAC() {
 #ifdef __APPLE__
     return 1;
 #endif
     return 0;
+}
+
+void insertSquare(int height, int width, int x, int y, ALLEGRO_COLOR color, ALLEGRO_DISPLAY *display) {
+    ALLEGRO_BITMAP *square = al_create_bitmap(width, height);
+    al_set_target_bitmap(square);
+    al_clear_to_color(color);
+    al_set_target_bitmap(al_get_backbuffer(display));
+    al_draw_bitmap(square, x, y, 0);
+    al_flip_display();
 }
