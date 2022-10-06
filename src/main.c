1116e4e51e02b64710d94c3a80bba8b01e683689
davisilvahenrique <davisilvahenrique@gmail.com>
2022-10-06 18:06:35 -0300

    .

--- src/main.c
+++ src/main.c
@@ -37,43 +37,133 @@ int main() {
     al_install_keyboard();
     al_register_event_source(event_queue, al_get_keyboard_event_source());
 
-    while (true) {
-        ALLEGRO_EVENT ev;
-        al_wait_for_event(event_queue, &ev);
+    int gamestate = 0; // STATE INICIAL
 
-        switch (ev.type) {
+    while (true) {
+
+        ALLEGRO_EVENT ev;
+        ALLEGRO_BITMAP *play = NULL, *options = NULL, *quit = NULL, *back = NULL;
+
+        al_wait_for_event(event_queue, &ev); // ESPERANDO POR EVENTOS
+
+        switch(ev.type) {
+
             case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN: {
 
-                if (ev.mouse.x >= 490 && ev.mouse.x <= 790) {
-                    if (ev.mouse.y >= 150 && ev.mouse.y <= 250) {     // play
-                        printf("start play\n");
-                    } else if (ev.mouse.y >= 300 && ev.mouse.y <= 400) {     // config
-                        printf("start config\n");
-                    } else if(ev.mouse.y >= 450 && ev.mouse.y <= 550) {             // quit
-                        killNine(timer, display, event_queue);
+                // EVENTO CLICK
+
+                switch(gamestate) {
+
+                    case 0: {
+                        // BOTÕES DO MENU
+
+                        if (ev.mouse.x >= 490 && ev.mouse.x <= 790) {
+                            if (ev.mouse.y >= 150 && ev.mouse.y <= 250) {
+                                // CLICK BOTÃO PLAY
+
+                                gamestate = 1;
+                            } else if (ev.mouse.y >= 300 && ev.mouse.y <= 400) {
+                                // CLICK BOTÃO CONFIG
+
+                                gamestate = 2;
+                            } else if (ev.mouse.y >= 450 && ev.mouse.y <= 550) {
+                                // CLICK BOTÃO QUIT
+
+                                al_destroy_bitmap(play);
+                                al_destroy_bitmap(options);
+                                al_destroy_bitmap(quit);
+                                al_destroy_bitmap(back);
+                                killNine(timer, display, event_queue);
+                                // ^^ SALVA SEU COMPUTADOR DE EXPLODIR
+                            }
+                        }
+
+                        break;
                     }
+                    case 1: {
+                        // BOTÕES DA TELA PLAY
+
+
+                        break;
+                    }
+                    case 2: {
+                        // BOTÕES DA TELA CONFIG
+
+                        if (ev.mouse.x >= 30 && ev.mouse.x <= 150 && ev.mouse.y >= 30 && ev.mouse.y <= 70){
+
+                            gamestate = 0; // RETORNA A TELA DE MENU
+                        }
+                        break;
+                    }
+
                 }
-                break;
+
             }
             case ALLEGRO_EVENT_TIMER: {
-                FPS_POLARITY = !FPS_POLARITY;
-                if (al_init_image_addon()) {
-//                    ALLEGRO_BITMAP *bg = al_load_bitmap("../src/assets/background.png");
-//                    al_draw_bitmap(bg, 0, 0, 0);
-                    ALLEGRO_BITMAP *play = al_load_bitmap("../src/assets/play.png");
-                    al_draw_bitmap(play, 490, 150, 0);
-                    ALLEGRO_BITMAP *options = al_load_bitmap("../src/assets/options.png");
-                    al_draw_bitmap(options, 490, 300, 0);
-                    ALLEGRO_BITMAP *quit = al_load_bitmap("../src/assets/quit.png");
-                    al_draw_bitmap(quit, 490, 450, 0);
+                FPS_POLARITY = !FPS_POLARITY; // POLARIDADE DO FPS
+
+                if (!al_init_image_addon()) break; // AGUARDA O ADDON DE IMAGEM INICIAR -> SE NÂO TIVER INICIADO NÃO PARTE PARA PRÓXIMA PARTE DO CÓDIGO
+
+                switch(gamestate) {
+
+                    case 0: {
+                        // TELA DO MENU
+
+                        al_clear_to_color(al_map_rgb(0, 0, 0));
+
+                        play = al_load_bitmap("../src/assets/play.png");
+                        if (play != NULL) {
+                            al_draw_bitmap(play, 490, 150, 0); // DRAW DO BOTÃO PLAY
+                        }
+                        options = al_load_bitmap("../src/assets/options.png");
+                        if (options != NULL) {
+                            al_draw_bitmap(options, 490, 300, 0); // DRAW DO BOTÃO OPTIONS
+                        }
+                        quit = al_load_bitmap("../src/assets/quit.png");
+                        if (quit != NULL) {
+                            al_draw_bitmap(quit, 490, 450, 0); // DRAW DO BOTÃO QUIT
+                        }
+
+                        break;
+                    }
+                    case 1: {
+                        // TELA PLAY
+
+                        al_clear_to_color(al_map_rgb(0, 0, 0));
+
+                        break;
+                    }
+                    case 2: {
+                        // TELA CONFIG
+
+                        al_clear_to_color(al_map_rgb(0, 0, 0));
+
+                        back = al_load_bitmap("../src/assets/back.png");
+                        if (back != NULL) {
+                            al_draw_bitmap(back, 30, 30, 0); // DRAW DO BOTÂO BACK
+                        }
+
+                        break;
+                    }
+
                 }
-                al_flip_display();
-                break;
+
+                al_flip_display(); // FLIP DISPLAY A TODO FRAME
+                break; // BREAK DO EVENTO TIMER
+
             }
             case ALLEGRO_EVENT_DISPLAY_CLOSE: {
+                al_destroy_bitmap(play);
+                al_destroy_bitmap(options);
+                al_destroy_bitmap(quit);
+                al_destroy_bitmap(back);
                 killNine(timer, display, event_queue);
                 break;
+                // ^^ SALVA SEU COMPUTADOR DE EXPLODIR
             }
+
         }
+
     }
+
 }
